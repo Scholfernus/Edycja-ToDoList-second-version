f34354a73cdd6e96141aeb83559f4529b933d839
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-17 11:42:39 +0200

    Poprawnie działający kod

--- Edit/editList/src/app/app-routing.module.ts
+++ Edit/editList/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list.components';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [{ path: '', redirectTo: '/home', pathMatch: 'full' },{ path: 'home', component: ListComponent },{ path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
