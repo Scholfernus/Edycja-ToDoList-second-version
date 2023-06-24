3986ce3f1e4be23d277697f07ad2ca78487dbd74
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-24 21:17:51 +0200

    Drobne poprawki component i HTML

--- Edit/editList/src/app/app.component.ts
+++ Edit/editList/src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component, ViewChild, ElementRef, Inject } from '@angular/core';
 import { TodoService } from './todo.service';
 
+interface Task {
+  name: string;
+  isFavorite: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,13 +14,13 @@ import { TodoService } from './todo.service';
 export class AppComponent {
   @ViewChild('editedTodoInput', { static: false }) editedTodoInput!: ElementRef<HTMLInputElement>;
 
-  todos: string[] = [];
+  todos: Task[] = [];
   newTodo: string = '';
   editedTodoIndex: number = -1;
   editedTodo: string = '';
 
   constructor(@Inject(TodoService) private todoService: TodoService) {
-    this.todos = this.todoService.getTodos();
+    this.todos = this.todoService.getTodos().map(todo => ({ name: todo, isFavorite: false }));
   }
 
   addTodo(): void {
@@ -26,23 +31,27 @@ export class AppComponent {
   }
 
   removeTodoIndex(index: number): void {
-    this.todoService.removeTodo(this.todos[index]);
+    this.todoService.removeTodo(this.todos[index].name);
   }
 
   editTodoIndex(index: number): void {
     this.editedTodoIndex = index;
-    this.editedTodo = this.todos[index];
+    this.editedTodo = this.todos[index].name;
   }
 
   saveTodoIndex(index: number): void {
-    this.todoService.editTodo(this.todos[index], this.editedTodo);
+    this.todoService.editTodo(this.todos[index].name, this.editedTodo);
     this.editedTodoIndex = -1;
     this.editedTodo = '';
   }
 
   removeAllTodos(): void {
     this.todoService.clearTodos();
-    this.todos = this.todoService.getTodos();
+    this.todos = [];
     this.newTodo = '';
+  }
+
+  addToFavorites(todo: Task): void {
+    todo.isFavorite = !todo.isFavorite;
   }
 }
